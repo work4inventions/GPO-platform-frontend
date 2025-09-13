3f26867aec3c3b39779f18630d6ac43870d1d7f2
ShubhamPaghadal <shubham10.tps@gmail.com>
2025-09-13 14:10:06 +0530

    Create a Login Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { HomeScreen } from "@/pages/home-screen";
 import { NotFound } from "@/pages/not-found";
 import { RouteProvider } from "@/providers/router-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
 import "@/styles/globals.css";
-
+import LoginPage from "./pages/loginPage";
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ThemeProvider>
             <BrowserRouter>
                 <RouteProvider>
                     <Routes>
-                        <Route path="/" element={<HomeScreen />} />
+                        <Route path="/" element={<LoginPage />} />
                         <Route path="*" element={<NotFound />} />
                     </Routes>
                 </RouteProvider>
